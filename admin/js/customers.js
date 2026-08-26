1baa61aa8e0198cf8feddb8d5ef8a9dbdbab942f
Shraboni <rashraboni69@gmail.com>
2026-08-26 21:18:58 +0600

    Update customers.js

--- admin/js/customers.js
+++ admin/js/customers.js
@@ -1,4 +1,4 @@
-const API_BASE = window.API_BASE || 'http://localhost:5000/api';
+const API_BASE = 'https://shosta-bazar-bd.onrender.com/api';
 const CUSTOMERS_API_URL = `${API_BASE}/customers`;
 let allCustomers = [];
 
