17286d1f81e4560269323067944cf56fdf76866c
Shraboni <rashraboni69@gmail.com>
2026-08-26 17:50:33 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 // API Configuration - Points to your Render backend
-const API_BASE = 'https://shosta-bazar-api.onrender.com/api';
+const API_BASE = 'https://shosta-bazar-bd.onrender.com/api';
